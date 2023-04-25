d5c848649b2f9219c4982553e85cde6b95b7d84e
jann-s <56644544+jann-s@users.noreply.github.com>
2023-04-25 16:47:16 +0200

    Add files via upload
    ORM for produkt objects added

---
+++ productservice/productmanager/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Produkt(models.Model):
+    name = models.CharField(max_length=200)
+    details = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2)
+    category_id = models.PositiveBigIntegerField()
+    def __str__(self):
+        return self.name
