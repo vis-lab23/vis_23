caa04512369af228a53f513a04f35c9fef4456d8
Selim Kaan Baykara <selim.k.baykara98@outlook.de>
2023-04-28 08:28:37 +0200

    Dockerize category service and extend docker-compose

--- category-service/src/app.module.ts
+++ category-service/src/app.module.ts
@@ -3,16 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Category } from './models/category.model';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 3306,
       username: 'root',
       password: 'c8de110f37300a53a971749',
       database: 'category',
+      retry: { max: 10 }, 
       models: [Category],
     }),
   ],
--- sql/initial.sql
+++ sql/initial.sql
@@ -2,3 +2,5 @@ insert into `role` (`level1`, `type`) values(0, 'admin');
 insert into `role` (`level1`, `type`) values(1, 'user');
 
 insert into `customer` (`name`, `lastname`, `password`, `username`, `role`) values('admin', 'admin', 'admin', 'admin', 1);
+
+CREATE DATABASE `category`;
