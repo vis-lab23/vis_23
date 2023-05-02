c55d360f297db68c11440aae89d5b94a8f65890a
Selim Kaan Baykara <selim.k.baykara98@outlook.de>
2023-05-02 11:45:39 +0200

    add integration and adjust dockerfile

--- src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/CategoryManagerImpl.java
+++ src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/CategoryManagerImpl.java
@@ -1,21 +1,48 @@
 package hska.iwi.eShopMaster.model.businessLogic.manager.impl;
 
-
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hska.iwi.eShopMaster.model.businessLogic.manager.CategoryManager;
 import hska.iwi.eShopMaster.model.database.dataAccessObjects.CategoryDAO;
 import hska.iwi.eShopMaster.model.database.dataobjects.Category;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 public class CategoryManagerImpl implements CategoryManager{
 	private CategoryDAO helper;
+	private HttpClient client;
+	private ObjectMapper objectMapper;
 	
 	public CategoryManagerImpl() {
 		helper = new CategoryDAO();
+		helper = new CategoryDAO();
+		client = HttpClient.newHttpClient();
+		objectMapper = new ObjectMapper();
 	}
 
 	public List<Category> getCategories() {
-		return helper.getObjectList();
+		HttpRequest request;
+		try {
+			request = HttpRequest.newBuilder()
+					.uri(new URI("http://cattegory-servcie:3000/category"))
+					.GET()
+					.build();
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			String body = response.body();
+			return objectMapper.readValue(body, new TypeReference<List<Category>>(){});
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public Category getCategory(int id) {
