82898ded8c2a499cc7bfdfb18054ea937b941726
linxside <git@linux-inside.de>
2023-05-02 20:13:51 +0200

    add openapi endpoint

--- category-service/src/main.ts
+++ category-service/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { Sequelize } from 'sequelize-typescript';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('Category API').setVersion('1.0.0').build();
+  const document = SwaggerModule.createDocument(app, config, {deepScanRoutes: true});
+  SwaggerModule.setup('openapi', app, document);
+
   await app.listen(3000);
 
   const sequelize = app.get(Sequelize);
