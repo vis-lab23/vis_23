c23dc2f1717d0e3ddeb3ab7e1bb8b36711164288
linxside <git@linux-inside.de>
2023-06-19 10:23:42 +0200

    improve selenium script

--- productservice/test/test.py
+++ productservice/test/test.py
@@ -3,45 +3,44 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
-#cregister
+#register
 driver.get('http://localhost:8888/EShop-1.0.0/')
 
-register_link = driver.find_element(By.LINK_TEXT, "Noch nicht registriert?")
-register_link.click()
+driver.find_element(By.LINK_TEXT, "Noch nicht registriert?").click()
 
-name_field = driver.find_element(By.NAME, "firstname").send_keys("test01")
-lastname_field = driver.find_element(By.NAME, "lastname").send_keys("test02")
-username_field = driver.find_element(By.NAME, "username").send_keys("test03")
-password_field = driver.find_element(By.NAME, "password1").send_keys("1234")
-password2_field = driver.find_element(By.NAME, "password2").send_keys("1234")
-register_button = driver.find_element(By.NAME, "method:execute")
-register_button.click()
+driver.find_element(By.NAME, "firstname").send_keys("test01")
+driver.find_element(By.NAME, "lastname").send_keys("test02")
+driver.find_element(By.NAME, "username").send_keys("test03")
+driver.find_element(By.NAME, "password1").send_keys("1234")
+driver.find_element(By.NAME, "password2").send_keys("1234")
+driver.find_element(By.NAME, "method:execute").click()
 
 #login
 driver.get('http://localhost:8888/EShop-1.0.0/')
 
-username_field = driver.find_element(By.NAME, "username").send_keys("test03")
-password_field = driver.find_element(By.NAME, "password").send_keys("1234")
-login_button = driver.find_element(By.NAME, "method:execute")
-login_button.click()
+driver.find_element(By.NAME, "username").send_keys("test03")
+driver.find_element(By.NAME, "password").send_keys("1234")
+driver.find_element(By.NAME, "method:execute").click()
+
+logout_button = driver.find_element(By.LINK_TEXT, "Logout")
+
+assert logout_button is not None
 
 #logout
-logout_link = driver.find_element(By.LINK_TEXT, "Logout")
-logout_link.click()
+logout_button.click()
+
 #TBD assert we are logged out
 
 #login as admin
 #login
 driver.get('http://localhost:8888/EShop-1.0.0/')
 
-username_field = driver.find_element(By.NAME, "username").send_keys("admin")
-password_field = driver.find_element(By.NAME, "password").send_keys("admin")
-login_button = driver.find_element(By.NAME, "method:execute")
-login_button.click()
+driver.find_element(By.NAME, "username").send_keys("admin")
+driver.find_element(By.NAME, "password").send_keys("admin")
+driver.find_element(By.NAME, "method:execute").click()
 
 #list all products
-list_link = driver.find_element(By.LINK_TEXT, "Alle Produkte")
-list_link.click()
+driver.find_element(By.LINK_TEXT, "Alle Produkte").click()
 
 # TBD
 #assert all products exist
