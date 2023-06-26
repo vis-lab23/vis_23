3b007b98a42d95af1a5ccd64e11f42f6ef55db03
jann-s <56644544+jann-s@users.noreply.github.com>
2023-06-26 21:20:49 +0200

    Update ProduktAnlegen.py

--- test-locust/ProduktAnlegen.py
+++ test-locust/ProduktAnlegen.py
@@ -8,11 +8,9 @@ class ProduktAnlegen(FastHttpUser):
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Cache-Control": "max-age=0",
         "Connection": "keep-alive",
-        "Cookie": "JSESSIONID=2D5D339999470D00B2371687DEA7DE36",
-        "Host": host,
-        "Referer": host + "/EShop-1.0.0/InitCategorySiteAction.action?pageToGoTo=p",
+        "Cookie": "JSESSIONID=76DD5CA09BCDB44AC6A8ED20CAC67F07",
+        "Host": "127.0.0.1:64180",
         "Sec-Fetch-Dest": "document",
         "Sec-Fetch-Mode": "navigate",
         "Sec-Fetch-Site": "same-origin",
@@ -27,19 +25,67 @@ class ProduktAnlegen(FastHttpUser):
     @task
     def t(self):
         with self.client.request(
-            "POST",
-            "/EShop-1.0.0/AddProductAction.action",
+            "GET",
+            "/EShop-1.0.0/InitCategorySiteAction.action?pageToGoTo=p",
             headers={
-                "Content-Length": "93",
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Origin": self.host,
+                "Referer": self.host + "/EShop-1.0.0/listAllProducts.action"
             },
-            data="name=1984&price=7.50&categoryId=b%C3%BCcher&details=Dystopie&method%3Aexecute=Hinzuf%C3%BCgen",
             catch_response=True,
         ) as resp:
             pass
         with self.client.request(
-            "GET", "/EShop-1.0.0/listAllProducts", catch_response=True
+            "POST",
+            "/EShop-1.0.0/AddProductAction.action",
+            headers={
+                "Cache-Control": "max-age=0",
+                "Content-Length": "104",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Origin": self.host,
+                "Referer": self.host + "/EShop-1.0.0/InitCategorySiteAction.action?pageToGoTo=p",
+            },
+            data="name=Herr+der+Ringe&price=9.50&categoryId=b%C3%BCcher&details=Klassiker&method%3Aexecute=Hinzuf%C3%BCgen",
+            catch_response=True,
+        ) as resp:
+            pass
+        with self.client.request(
+            "GET",
+            "/EShop-1.0.0/listAllProducts",
+            headers={
+                "Cache-Control": "max-age=0",
+                "Referer": self.host + "/EShop-1.0.0/InitCategorySiteAction.action?pageToGoTo=p",
+            },
+            catch_response=True,
+        ) as resp:
+            pass
+        with self.client.request(
+            "GET",
+            "/EShop-1.0.0/InitCategorySiteAction.action?pageToGoTo=p",
+            headers={"Referer": self.host + "/EShop-1.0.0/listAllProducts"},
+            catch_response=True,
+        ) as resp:
+            pass
+        with self.client.request(
+            "POST",
+            "/EShop-1.0.0/AddProductAction.action",
+            headers={
+                "Cache-Control": "max-age=0",
+                "Content-Length": "109",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Origin": self.host,
+                "Referer": self.host + "/EShop-1.0.0/InitCategorySiteAction.action?pageToGoTo=p",
+            },
+            data="name=Moby+Dick&price=2.50&categoryId=b%C3%BCcher&details=Ebenfalls+Klassiker&method%3Aexecute=Hinzuf%C3%BCgen",
+            catch_response=True,
+        ) as resp:
+            pass
+        with self.client.request(
+            "GET",
+            "/EShop-1.0.0/listAllProducts",
+            headers={
+                "Cache-Control": "max-age=0",
+                "Referer": self.host + "/EShop-1.0.0/InitCategorySiteAction.action?pageToGoTo=p",
+            },
+            catch_response=True,
         ) as resp:
             pass
 
